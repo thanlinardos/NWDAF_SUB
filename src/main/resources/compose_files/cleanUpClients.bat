abb64db9885fc8cbbb07e7e92af953f6b5827075
thanlinardos <thanlinardos@yahoo.gr>
2023-10-08 22:38:55 +0300

    clean up

--- src/main/resources/compose_files/cleanUpClients.bat
+++
@@ -1,6 +0,0 @@
-for /l %%x in (10000, 1, 10004) do (
-   echo %%x
-   docker compose -f docker-compose-client.yml -p dockprom down
-)
-docker compose down
-docker compose -f C:\Users\thanl\Documents\eclipse-workspace-spring\NWDAF_SUB_CLIENT\dockprom\docker-compose.yml down
--- src/main/resources/compose_files/manyclients.bat
+++
@@ -1,7 +0,0 @@
-docker compose -f C:\Users\thanl\Documents\eclipse-workspace-spring\NWDAF_SUB_CLIENT\dockprom\docker-compose.yml up -d
-docker compose up -d
-for /l %%x in (10001, 1, 10004) do (
-   echo %%x
-   (echo client_port=%%x && echo server_port=8080 && echo server_host=http://nwdafSub && echo client_host=http://nwdafSubClient && echo client_max_port=8085 && echo server_max_port=8070 && echo c=%%x)> ./temp.env
-   docker compose -f docker-compose-client.yml --env-file ./temp.env -p client_%%x up -d
-)
