487d08b4dd41e4c74e6706eb1fb71480fba5ac9b
thanlinardos <thanlinardos@yahoo.gr>
2023-11-08 23:22:45 +0200

    automated certs

--- src/main/resources/certificates/generateCertificates.sh
+++ src/main/resources/certificates/generateCertificates.sh
@@ -3,15 +3,35 @@
 echo "Generating certificates" &&
 SAN_VALUES=$(sed 's/.*/dns:&/' sanHosts.txt | tr '\n' ',' | sed 's/,$//') &&
 SAN_CLIENT_VALUES=$(sed 's/.*/dns:&/' sanClientHosts.txt | tr '\n' ',' | sed 's/,$//') &&
+
+name=test &&
+password=123456 &&
+org_unit=dev &&
+org=org &&
+city=city &&
+state=state &&
+country_code=gr &&
+
+client_name=test &&
+client_password=123456 &&
+client_org_unit=dev &&
+client_org=org &&
+client_city=city &&
+client_state=state &&
+client_country_code=gr &&
+rm local-ssl.p12 local-client-ssl.p12 local-cert.crt local-client-cert.crt
 echo "Generating server certificate" &&
-keytool -genkeypair -alias local_ssl -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore local-ssl.p12 -validity 365 -ext "san=$SAN_VALUES" &&
-openssl pkcs12 -in local-ssl.p12 -clcerts -nokeys -out local-ssl.crt &&
+keytool -genkeypair -alias local_ssl -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore local-ssl.p12 -storepass $password -dname "CN=$name, OU=$org_unit, O=$org, L=$city, ST=$state, C=$country_code" -validity 365 -ext "san=$SAN_VALUES" &&
+openssl pkcs12 -in local-ssl.p12 -clcerts -nokeys -out local-cert.crt -passin pass:$password &&
 echo "Generating client certificate" &&
-keytool -genkeypair -alias local_client_ssl -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore local-client-ssl.p12 -validity 365 -ext "san=$SAN_CLIENT_VALUES" &&
-openssl pkcs12 -in local-client-ssl.p12 -clcerts -nokeys -out local-client-ssl.crt &&
+keytool -genkeypair -alias local_client_ssl -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore local-client-ssl.p12 -storepass $client_password -dname "CN=$client_name, OU=$client_org_unit, O=$client_org, L=$client_city, ST=$client_state, C=$client_country_code" -validity 365 -ext "san=$SAN_CLIENT_VALUES" &&
+openssl pkcs12 -in local-client-ssl.p12 -clcerts -nokeys -out local-client-cert.crt -passin pass:$client_password &&
 echo "Done" &&
 echo "Cleaning up..." &&
 rm sanHosts.txt &&
 rm sanClientHosts.txt &&
-cp local-ssl.p12 local-client-ssl.p12 local-ssl.crt local-client-ssl.crt ../../../../../NWDAF_SUB_CLIENT/src/main/resources/certificates
+cd ../../../../../NWDAF_SUB_CLIENT/src/main/resources/certificates &&
+rm local-ssl.p12 local-client-ssl.p12 local-cert.crt local-client-cert.crt
+cd - &&
+cp local-ssl.p12 local-client-ssl.p12 local-cert.crt local-client-cert.crt ../../../../../NWDAF_SUB_CLIENT/src/main/resources/certificates &&
 echo "Finished"
--- src/main/resources/certificates/installCertificatesChrome.sh
+++ src/main/resources/certificates/installCertificatesChrome.sh
@@ -1,13 +1,21 @@
 echo "Installing the certificates to local machine & chrome..." &&
 
-sudo chmoc +777 local-cert.crt &&
-sudo chmoc +777 local-client-cert.crt &&
-sudo cp local-cert.crt /usr/local/share/ca-certificates/ && 
-sudo cp local-client-cert.crt /usr/local/share/ca-certificates/ && 
+sudo chmod +777 local-cert.crt &&
+sudo chmod +777 local-client-cert.crt &&
+cd /usr/local/share/ca-certificates/ &&
+rm local-client-cert.crt local-cert.crt -y
+cd - &&
+sudo cp local-client-cert.crt local-cert.crt /usr/local/share/ca-certificates/ && 
 sudo update-ca-certificates && 
 sudo apt-get update && 
 sudo apt-get install libnss3-tools &&
+
+while read -r -d $'\0' i ; do    sudo certutil -d 'sql:'"$i" -D -n local_cert; done < <(sudo find "$HOME" -type f -iregex '.*[/]cert[89][.]db' -printf '%h\0') &&
+while read -r -d $'\0' i ; do    sudo certutil -d 'sql:'"$i" -D -n local_client_cert; done < <(sudo find "$HOME" -type f -iregex '.*[/]cert[89][.]db' -printf '%h\0') &&
+
+echo "Removed existing certificates from chrome" &&
+
 while read -r -d $'\0' i ; do    sudo certutil -d 'sql:'"$i" -A -t "C,," -n local_cert -i local-cert.crt; done < <(sudo find "$HOME" -type f -iregex '.*[/]cert[89][.]db' -printf '%h\0') && 
 while read -r -d $'\0' i ; do    sudo certutil -d 'sql:'"$i" -A -t "C,," -n local_client_cert -i local-client-cert.crt; done < <(sudo find "$HOME" -type f -iregex '.*[/]cert[89][.]db' -printf '%h\0') &&
 
-echo "Done"
+echo "Added new certificates"
