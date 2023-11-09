c56608ae38b2efb374df2f68e1889baf84e96b0f
thanlinardos <thanlinardos@yahoo.gr>
2023-11-09 19:20:30 +0200

    fix certs & split compose files

--- src/main/resources/certificates/installCertificatesChrome.sh
+++ src/main/resources/certificates/installCertificatesChrome.sh
@@ -15,7 +15,7 @@ while read -r -d $'\0' i ; do    sudo certutil -d 'sql:'"$i" -D -n local_client_
 
 echo "Removed existing certificates from chrome" &&
 
-while read -r -d $'\0' i ; do    sudo certutil -d 'sql:'"$i" -A -t "C,," -n local_cert -i local-cert.crt; done < <(sudo find "$HOME" -type f -iregex '.*[/]cert[89][.]db' -printf '%h\0') && 
-while read -r -d $'\0' i ; do    sudo certutil -d 'sql:'"$i" -A -t "C,," -n local_client_cert -i local-client-cert.crt; done < <(sudo find "$HOME" -type f -iregex '.*[/]cert[89][.]db' -printf '%h\0') &&
+while read -r -d $'\0' i ; do    sudo certutil -d 'sql:'"$i" -A -t "CT,C,C" -n local_cert -i local-cert.crt; done < <(sudo find "$HOME" -type f -iregex '.*[/]cert[89][.]db' -printf '%h\0') && 
+while read -r -d $'\0' i ; do    sudo certutil -d 'sql:'"$i" -A -t "CT,C,C" -n local_client_cert -i local-client-cert.crt; done < <(sudo find "$HOME" -type f -iregex '.*[/]cert[89][.]db' -printf '%h\0') &&
 
 echo "Added new certificates"
