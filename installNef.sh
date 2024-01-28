4593431665a9fa2f45fe6f44aeba2d35f6f910d4
thanlinardos <thanlinardos@yahoo.gr>
2024-01-28 18:27:50 +0200

    add patch for installing nef

--- installNef.sh
+++ installNef.sh
@@ -1,4 +1,6 @@
 cd ../nwdaf_sub_collector &&
 git clone https://github.com/medianetlab/NEF_emulator &&
+cd NEF_emulator &&
+patch -p1 < ../../NWDAF_SUB/scripts/customize_nef_emulator.patch &&
 rm -rf .git &&
 cd -
