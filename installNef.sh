d3effd106026353b463ddc6eac89a319dc019267
thanlinardos <thanlinardos@yahoo.gr>
2024-01-28 18:35:24 +0200

    add patch for installing nef

--- installNef.sh
+++ installNef.sh
@@ -3,4 +3,9 @@ git clone https://github.com/medianetlab/NEF_emulator &&
 cd NEF_emulator &&
 patch -p1 < ../../NWDAF_SUB/scripts/customize_nef_emulator.patch &&
 rm -rf .git &&
+make prepare-dev-env &&
+make build &&
+make up &&
+make db-init &&
+make down &&
 cd -
